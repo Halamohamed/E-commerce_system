7727c89e87d0cd2f38f713800fec49402bb01050
Halamohamed <halaalimohammed@gmail.com>
2026-02-25 10:38:27 +0100

    updated order

--- src/main/java/se/lexicon/ecommerce_system/entities/Order.java
+++ src/main/java/se/lexicon/ecommerce_system/entities/Order.java
@@ -1,23 +1,56 @@
 package se.lexicon.ecommerce_system.entities;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.time.Instant;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
+@Table(name = "orders")
 public class Order {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "order_date", nullable = false, updatable = false)
     private Instant orderDate;
 
     @Enumerated(EnumType.STRING)
     private OrderStatus status;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "customer_id")
+    @JoinColumn(name = "customer_id", nullable = false)
     private Customer customer;
 
+    @OneToMany(
+            mappedBy = "order",
+            cascade = CascadeType.ALL,
+            orphanRemoval = true,
+            fetch = FetchType.LAZY
+    )
+    private List<OrderItem> items;
+
+    @PrePersist
+    public void prePersist() {
+        orderDate = Instant.now();
+    }
+
+    public void addItem(OrderItem item) {
+        items.add(item);
+        item.setOrder(this);
+    }
+    public void removeItem(OrderItem item) {
+        items.remove(item);
+        item.setOrder(null);
+    }
 
 
 }
