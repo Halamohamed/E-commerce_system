886486ec926ccdf5736dd7d71e76b933b0d6351d
Halamohamed <halaalimohammed@gmail.com>
2026-02-25 10:39:18 +0100

    added Category

---
+++ src/main/java/se/lexicon/ecommerce_system/entities/Category.java
@@ -0,0 +1,24 @@
+package se.lexicon.ecommerce_system.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false,length = 150)
+    private String name;
+    @OneToMany(mappedBy = "category")
+    private List<Product> products;
+}
