f4421dc6fce3a0023c3c73712afc3d398789130d
Halamohamed <halaalimohammed@gmail.com>
2026-02-25 10:38:03 +0100

    added OrderItem

---
+++ src/main/java/se/lexicon/ecommerce_system/entities/OrderItem.java
@@ -0,0 +1,33 @@
+package se.lexicon.ecommerce_system.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "order_items")
+public class OrderItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Integer quantity;
+    @Column(name = "price" ,nullable = false)
+    private BigDecimal priceAtPurchase;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+
+}
