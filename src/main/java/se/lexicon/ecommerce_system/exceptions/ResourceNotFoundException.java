43b071965bf0fd329ffa09122a2f69da1e43a7ec
Halamohamed <halaalimohammed@gmail.com>
2026-03-04 13:54:38 +0100

    added ResourceNotFoundException

---
+++ src/main/java/se/lexicon/ecommerce_system/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,8 @@
+package se.lexicon.ecommerce_system.exceptions;
+
+public class ResourceNotFoundException extends Exception{
+
+    public ResourceNotFoundException(String message){
+        super(message);
+    }
+}
