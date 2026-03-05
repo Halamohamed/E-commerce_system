37a6ef680dc0ae5ee253634a5732252599bc30a4
Halamohamed <halaalimohammed@gmail.com>
2026-03-05 13:14:10 +0100

    updated ProductServiceImpl

--- src/main/java/se/lexicon/ecommerce_system/service/ProductService.java
+++ src/main/java/se/lexicon/ecommerce_system/service/ProductService.java
@@ -2,12 +2,13 @@ package se.lexicon.ecommerce_system.service;
 
 import se.lexicon.ecommerce_system.DTOs.productDTO.ProductRequest;
 import se.lexicon.ecommerce_system.DTOs.productDTO.ProductResponse;
+import se.lexicon.ecommerce_system.exceptions.ResourceNotFoundException;
 
 import java.util.List;
 
 public interface ProductService {
 
-    public ProductResponse create(ProductRequest request);
+    public ProductResponse create(ProductRequest request) throws ResourceNotFoundException;
     public List<ProductResponse> findAll();
     public List<ProductResponse> searchByName(String name);
 
--- src/main/java/se/lexicon/ecommerce_system/service/impl/ProductServiceImpl.java
+++ src/main/java/se/lexicon/ecommerce_system/service/impl/ProductServiceImpl.java
@@ -1,8 +1,13 @@
 package se.lexicon.ecommerce_system.service.impl;
 
+
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import se.lexicon.ecommerce_system.DTOs.productDTO.ProductRequest;
 import se.lexicon.ecommerce_system.DTOs.productDTO.ProductResponse;
+import se.lexicon.ecommerce_system.entities.Category;
+import se.lexicon.ecommerce_system.entities.Product;
+import se.lexicon.ecommerce_system.exceptions.ResourceNotFoundException;
 import se.lexicon.ecommerce_system.mapper.ProductMapper;
 import se.lexicon.ecommerce_system.repositories.CategoryRepository;
 import se.lexicon.ecommerce_system.repositories.CustomerRepository;
@@ -24,17 +29,30 @@ public class ProductServiceImpl implements ProductService {
     }
 
     @Override
-    public ProductResponse create(ProductRequest request) {
-        return null;
+    @Transactional
+    public ProductResponse create(ProductRequest request) throws ResourceNotFoundException {
+        Category category = categoryRepository.findById(request.categoryId())
+                .orElseThrow(() -> new ResourceNotFoundException("Category Not Found"));
+        Product product = productMapper.toEntity(request, category);
+        Product savedProduct = productRepository.save(product);
+        return productMapper.toResponse(savedProduct);
     }
 
     @Override
+    @Transactional(readOnly = true)
     public List<ProductResponse> findAll() {
-        return List.of();
+        return productRepository.findAll().stream()
+                .map(productMapper::toResponse)
+                .toList();
     }
 
     @Override
+    @Transactional(readOnly = true)
     public List<ProductResponse> searchByName(String name) {
-        return List.of();
+
+        return productRepository.findByNameContainingIgnoreCase(name)
+                .stream()
+                .map(productMapper::toResponse)
+                .toList();
     }
 }
