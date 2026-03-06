41618caeb63e2148dfe5b9355708109680d284b4
Halamohamed <halaalimohammed@gmail.com>
2026-03-06 13:54:15 +0100

    added DuplicateKeyException

---
+++ src/main/java/se/lexicon/ecommerce_system/exceptions/DuplicateKeyException.java
@@ -0,0 +1,8 @@
+package se.lexicon.ecommerce_system.exceptions;
+
+public class DuplicateKeyException extends Exception {
+
+    public DuplicateKeyException(String message) {
+        super(message);
+    }
+}
